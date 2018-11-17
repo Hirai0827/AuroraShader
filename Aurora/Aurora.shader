43a53488f5d1604fe6cee8bacfa69fb5d3b56f9c
Hirai0827 <41163700+Hirai0827@users.noreply.github.com>
2018-11-18 02:41:34 +0900

    add asset file

---
+++ Aurora/Aurora.shader
@@ -0,0 +1,49 @@
+﻿Shader "Custom/Aurora"
+{
+    Properties {
+        _MainTex ("Albedo (RGB)", 2D) = "white" {}
+		_Color("Top Color",color) = (1.0,1.0,1.0)
+		_Color2("Bottom Color",color) = (1.0,1.0,1.0)
+    }
+    SubShader {
+        Tags { "Queue" = "Transparent" }
+        LOD 200
+        
+        CGPROGRAM
+        #pragma surface surf Lambert vertex:vert alpha:fade 
+		#pragma target 3.0
+
+        sampler2D _MainTex;
+
+        struct Input {
+            float2 uv_MainTex;
+			float4 color : COLOR;
+			float3 objpos;
+			float3 worldNormal;
+      		float3 viewDir;
+        };
+		float4 _Color;
+		float4 _Color2;
+        void vert(inout appdata_full v, out Input o )
+        {
+            UNITY_INITIALIZE_OUTPUT(Input, o);
+            float amp = 0.15*sin(_Time*10 + v.vertex.x * 3) * (2 + 1 * sin(_Time + v.vertex.x * 0.01));
+			float amp2 = 0.15 * sin(_Time*7.5 + v.vertex.x * 2)* (3 + 1.5 * sin(_Time + v.vertex.x * 0.01));
+			float3 delta = v.normal;
+            v.vertex.xyz = v.vertex.xyz + float3(0,lerp(amp,amp2,v.vertex.z),0); 
+			o.objpos = v.vertex.xyz;
+            //v.normal = normalize(float3(v.normal.x+offset_, v.normal.y, v.normal.z));
+        }
+
+        void surf (Input IN, inout SurfaceOutput o) {
+            fixed4 c = tex2D (_MainTex, IN.uv_MainTex);
+			fixed2 uv = IN.uv_MainTex;
+			fixed4 rimColor  = fixed4(0.5,0.7,0.5,1);
+            o.Albedo = lerp(_Color,_Color2,uv.y);
+            o.Alpha = 0.4 * (1 -4 * pow(0.5-uv.y,2));
+			float rim = (1 - saturate(dot(IN.viewDir, o.Normal))) * 3;
+     		o.Emission = lerp(_Color,_Color2,uv.y) * pow(rim, 2) * 2;
+        }
+        ENDCG
+    }
+}
---
+++ Aurora/MeshDeformer.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshDeformer : MonoBehaviour
+{
+
+    Mesh deformingMesh;
+    Vector3[] originalVertices, displacedVertices;
+    Vector3[] vertexVelocities;
+
+    public float springForce = 10f;
+    public float damping = 5f;
+
+    private float minMag;
+    private Vector3 minVector3;
+
+    // Use this for initialization
+    void Start()
+    {
+        /*deformingMesh = GetComponent<MeshFilter>().mesh;
+		originalVertices = deformingMesh.vertices;
+		displacedVertices = new Vector3[originalVertices.Length];
+		for (int i = 0; i < originalVertices.Length; i++) {
+			displacedVertices[i] = originalVertices[i];
+		}
+		vertexVelocities = new Vector3[originalVertices.Length];*/
+    }
+
+    public void StoreMesh()
+    {
+        deformingMesh = GetComponent<MeshFilter>().mesh;
+        originalVertices = deformingMesh.vertices;
+        displacedVertices = new Vector3[originalVertices.Length];
+        for (int i = 0; i < originalVertices.Length; i++)
+        {
+            displacedVertices[i] = originalVertices[i];
+        }
+        vertexVelocities = new Vector3[originalVertices.Length];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < displacedVertices.Length; i++)
+        {
+            UpdateVertex(i);
+        }
+        deformingMesh.vertices = displacedVertices;
+        deformingMesh.RecalculateNormals();
+    }
+
+    void UpdateVertex(int i)
+    {
+        Vector3 velocity = vertexVelocities[i];
+        Vector3 displacement = displacedVertices[i] - originalVertices[i];
+        velocity -= displacement * springForce * Time.deltaTime;
+        velocity *= 1f - damping * Time.deltaTime;
+        vertexVelocities[i] = velocity;
+        displacedVertices[i] += velocity * Time.deltaTime;
+    }
+
+    public void AddDeformingForce(Vector3 point, float velocity)
+    {
+
+        point = transform.InverseTransformPoint(point);
+        for (int i = 0; i < displacedVertices.Length; i++)
+        {
+            AddForceToVertex(i, point, velocity);
+        }
+    }
+
+    private void AddForceToVertex(int i, Vector3 point, float velocity)
+    {
+        Vector3 pointToVertex = displacedVertices[i] - point;
+        Vector3 pointToOriginalVertex = originalVertices[i] - point;
+        velocity = velocity / (1f + 10000 * pointToVertex.sqrMagnitude);
+        velocity /= Time.deltaTime;
+        vertexVelocities[i] += pointToVertex.normalized * velocity;
+    }
+}
+
